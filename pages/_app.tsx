7f4430af83cd28b8b8d749843336eb33c789449a
nguyen-william93 <=>
2022-09-24 12:29:25 -0500

    initiualize header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.scss'
+import React, {useState, useEffect} from 'react'
+import {Layout} from '../components'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,14 @@ const posts=[
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post , index) => ( <PostCard post={post} key={post.title} />))}
+          {posts.map((post) => ( <PostCard post={post} key={post.title} />))}
         </div>
       </div>
       <div className='lg:col-span-4 col-span-1'>
